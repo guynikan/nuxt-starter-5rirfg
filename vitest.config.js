c98ee1a18c13c0063e51251e96218f448d6b1f9e
guynikan <36284169+guynikan@users.noreply.github.com>
2023-05-03 20:14:07 -0300

    1 file modified

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineVitestConfig({
   plugins: [vue()],
   test: {
+    environment: 'nuxt',
     setupFiles: ['./tests/setup.ts'],
   },
   resolve: {
