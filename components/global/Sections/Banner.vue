eb97f3c836c1bd4560d2b4d1b76cb1883131ca2d
guynikan <36284169+guynikan@users.noreply.github.com>
2023-05-03 17:39:14 -0300

    2 files modified, 1 added

---
+++ components/global/Sections/Banner.vue
@@ -0,0 +1,12 @@
+<script setup>
+const route = useRoute();
+const params = {
+  slug: route.name,
+};
+const { data } = await useSanityQuery(pageQuery, params);
+</script>
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+</template>
